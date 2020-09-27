090759d871b371e02c4bc9c345164b5b884eec0f
meng <1742451174@qq.com>
2020-09-27 17:33:02 +0800

    查找共同祖先

---
+++ src/org/meng/letcode/day02/Solution.java
@@ -0,0 +1,45 @@
+package org.meng.letcode.day02;
+
+import javax.swing.*;
+
+/**
+ * @author meng
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+     *
+     * @param root 二叉树根
+     * @param p    节点1
+     * @param q    节点2
+     * @return 最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > q.val) {
+            if (root.val <= p.val && root.val <= q.val) {
+                return root;
+            } else {
+
+            }
+
+        } else {
+
+        }
+    }
+
+
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
